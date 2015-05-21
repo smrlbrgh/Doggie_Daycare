24a6be4cb1dde0e8c00f7c83dd8ff40c68968364
Sabrina L. Marlbrough <smarlbrough@live.com>
2015-05-21 11:46:55 -0500

    fixed path name for ImageMagick and fixed index errors with dogs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   #Giving paperclip access to ImageMagick
-  Paperclip.options[:command_path] = "/c/Program Files/ImageMagick-6.9.1-Q16"
+  Paperclip.options[:command_path] = "C:/Program Files/ImageMagick-6.9.1-Q16;C:/Program Files (x86)/GnuWin32/bin;"
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
