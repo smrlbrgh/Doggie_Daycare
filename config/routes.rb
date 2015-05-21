9c199c78dd84060efcc0743afc978f3cdba398f3
Sabrina L. Marlbrough <smarlbrough@live.com>
2015-05-21 09:32:38 -0500

    generate and route welcome page and add starter style flatly

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+
+  get 'pages/about'
+
   resources :dogs
 
   resources :owners
@@ -7,7 +11,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
