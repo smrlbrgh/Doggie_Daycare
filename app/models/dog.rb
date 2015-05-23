531725dceaef4c079ad51cf351fad8b38f186107
Sabrina L. Marlbrough <smarlbrough@live.com>
2015-05-23 13:48:10 -0500

    added validations and table class

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,6 +1,10 @@
 class Dog < ActiveRecord::Base
   belongs_to :owner
   belongs_to :breed
+
+
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
  validates_attachment_content_type :avatar, :content_type =>/\Aimage\/.*\Z/
+ validates :name, :breed_id, :dob, :vet, :owner_id, presence: true
+
 end
