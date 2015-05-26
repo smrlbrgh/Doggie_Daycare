9e8c70d77b8730ea3f1ca1ece929c040bebe3713
Sabrina L. Marlbrough <smarlbrough@live.com>
2015-05-26 08:37:26 -0500

    changed avatar styles to be square

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,10 +3,18 @@ class Dog < ActiveRecord::Base
   belongs_to :owner
   belongs_to :breed
 
+ validates :name, :breed_id, :dob, :vet, :owner_id,
+ presence: true
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
- validates_attachment_content_type :avatar, :content_type =>/\Aimage\/.*\Z/
- validates :name, :breed_id, :dob, :vet, :owner_id, presence: true
+  has_attached_file :avatar, :styles => {
+    :medium => "300x300#",
+    :thumb => "100x100#"
+  },
+    :default_url => "/images/:style/missing.png"
+ validates_attachment_content_type :avatar,
+ :content_type =>/\Aimage\/.*\Z/
 
+ validates_attachment_size :avatar, :less_than => 3.megabytes,
+  :unless => Proc.new {|m|  m[:avatar_file_name].blank?}
 
 end
