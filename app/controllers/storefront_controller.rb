b319a7dd91aa5f5ac51180e3bd82f78536755c1b
Sabrina L. Marlbrough <smarlbrough@live.com>
2015-06-02 08:48:50 -0500

    Added navbar links to shop and shop categories and filtered all items by category

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -1,5 +1,11 @@
 class StorefrontController < ApplicationController
   def all_items
-    @products = Product.all
+    # TODO: Add logic to handle when cat id returns nil(do in controller)
+    if params[:cat_id]
+      @category = Category.find(params[:cat_id])
+      @products = Product.where(category_id: params[:cat_id])
+    else
+      @products = Product.all
+    end
   end
 end
