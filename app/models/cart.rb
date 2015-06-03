8b4e0e1e07e708ae6bcd5db661d5001f782ed91a
Sabrina L. Marlbrough <smarlbrough@live.com>
2015-06-03 10:51:42 -0500

    added ordering controls

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,6 +4,11 @@ class Cart < ActiveRecord::Base
   def subtotal
     line_items.to_a.sum { |item| item.total_price }
   end
+
+  def qty_total
+    line_items.to_a.sum { |item| item.quantity}
+  end
+
   def add_product(product_id)
     current_item = line_items.find_by(product_id: product_id)
     if current_item
